053eada9701d3c7d2f9ed5d35e2fed788b0a897d
Antón <anton.casais.mera@gmail.com>
2022-03-28 10:02:08 +0200

    Fix service object

--- app/services/crm/api_provider.rb
+++ app/services/crm/api_provider.rb
@@ -1,12 +1,14 @@
-class ApiProvider
-	class << self
-		def oauth(type)
-			case type
-			when "hubspot"
-				Hubspot::Oauth
-			else
-				raise StandardError.new "Error Oauth: type has an invalid value (#{type})"
-			end
-		end
-	end
+module CRM
+  class ApiProvider
+    class << self
+      def oauth(type)
+        case type
+        when "hubspot"
+          Hubspot::Oauth
+        else
+          raise StandardError.new "Error Oauth: type has an invalid value (#{type})"
+        end
+      end
+    end
+  end
 end
